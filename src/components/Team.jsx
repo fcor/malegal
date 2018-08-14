c4a725a0216e77b597dfce7c7fbbe48fcdae429f
Fabio Cortés <fcor14@hotmail.com>
2018-08-14 10:51:13 -0500

    Team ok

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -10,12 +10,30 @@ const teamMembers = ['Daniela',
                     ]
 
 class Team extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isShowingDetails: false,
+      member: null
+    }
+
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onClick(member){
+    // console.log(member);
+    this.setState({
+      isShowingDetails: true,
+      member: member
+    })
+  }
+
   render() {
     return(
       <div className="container">
         <div className="team-box column">
           <Header />
-          <Cards />
+          <Cards onClick={this.onClick} />
         </div>
       </div>
     )
@@ -27,12 +45,15 @@ const Header = () =>
     NUESTRO EQUIPO
   </h1>
 
-const Cards = () =>
+const Cards = ({ onClick }) =>
   <div className="team-cards">
     {teamMembers.map( (item) => {
       const member = getMemberInfo(item)
       return (
-        <div className="team-card column">
+        <div
+          key={item}
+          onClick={() => onClick(item)}
+          className="team-card column">
           <img
             src={member.avatar}
             alt="avatar"
--- src/utils/team.js
+++ src/utils/team.js
@@ -2,7 +2,7 @@ import man from '../assets/img/man.png'
 import woman from '../assets/img/woman.png'
 
 const getMemberInfo = (name) => {
-  if (name == 'Daniela') {
+  if (name === 'Daniela') {
     return {
       name: 'Daniela Guerrero Ordoñéz',
       position: 'Asociada',
@@ -10,7 +10,7 @@ const getMemberInfo = (name) => {
       email: 'daniela@malegal.co',
       bio: 'Abogada de la Universidad Católica de Colombia, con experiencia en litigio y contratación estatal, con profundización en derecho laboral, seguridad social y derecho administrativo. Como miembro del equipo de litigios en materia de seguridad social y laboral ejerce funciones de apoyo en la sustanciación de memoriales, contestación de demandas, y asistencia a audiencias. '
     }
-  } else if (name == 'Maria') {
+  } else if (name === 'Maria') {
     return {
       name: 'María Alejandra Giraldo',
       position: 'Asociada Senior',
@@ -18,7 +18,7 @@ const getMemberInfo = (name) => {
       email: 'maria@malegal.co',
       bio: 'Maria Alejandra es Abogada, LLM en Derecho Financiero y Bancario, con una amplia experiencia en temas de gobierno corporativo y compliance de las entidades financieras, negociación de contratos financieros, mercado de valores y regulación de servicios y productos bancarios. Coordina los esfuerzos del equipo en materia de transacciones estructuradas y facilidades crediticias, así como la consultoría legal de la firma en materia de derecho financiero y regulación cambiaria. '
     }
-  } else if (name == 'Ana') {
+  } else if (name === 'Ana') {
     return {
       name: 'Ana Ortiz',
       position: 'Asociada Senior',
@@ -26,7 +26,7 @@ const getMemberInfo = (name) => {
       email: 'ana@malegal.co',
       bio: 'Abogada de la Universidad Sergio Arboleda, con énfasis en Derecho Anglosajón, hace parte del equipo de litigios en materia de seguridad social y laboral. Dentro de sus funciones se encuentra la preparación, presentación y contestación de demandas, asistencia a audiencias y diligencias judiciales. Adicionalmente, apoya en labores de investigación y consultoría en materia Pensional.'
     }
-  } else if (name == 'Natalia') {
+  } else if (name === 'Natalia') {
     return {
       name: 'Natalia García Pulido',
       position: 'Asociada',
@@ -34,7 +34,7 @@ const getMemberInfo = (name) => {
       email: 'natalia@malegal.co',
       bio: 'Abogada de la Universidad Externado de Colombia, con profundización en derecho civil, derecho de seguros y contratos atípicos. Como miembro del equipo de derecho financiero participa en la revisión y negociación de contratos de diferentes transacciones y financiación de proyectos de infraestructura. Así mismo, apoya al grupo de derecho corporativo y financiero con labores de investigación y consultoría.'
     }
-  } else if (name == 'Edgar') {
+  } else if (name === 'Edgar') {
     return {
       name: 'Edgar Muñoz',
       position: 'Socio',
@@ -42,7 +42,7 @@ const getMemberInfo = (name) => {
       email: 'edgar@malegal.co',
       bio: 'Abogado, LLM, MBA, con más de 18 años de experiencia en temas relacionados con inversiones, derecho corporativo, bancario, y de valores, banca de inversión, fondos de inversión colectiva y fondos de capital privado; operaciones de tesorería y negociación de créditos sindicados. Cuenta con una amplia experiencia profesional en estos campos, tanto en el sector público como privado a través de su vinculación a firmas de abogados de primer nivel global y local, Citibank, Davivienda, y el Ministerio de Hacienda y Crédito Público, entre otros.'
     }
-  } else if (name == 'Laura') {
+  } else if (name === 'Laura') {
     return {
       name: 'Laura Villareal',
       position: 'Asociada',
