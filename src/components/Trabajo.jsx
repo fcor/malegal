7120d89490d93224793e32ebc81fa48313dc01df
Fabio Cortés <fcor14@hotmail.com>
2018-08-16 18:47:09 -0500

    Form ok

--- src/components/Trabajo.jsx
+++ src/components/Trabajo.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
 import SVG from './SVG'
 
+const getInputDetails = (type) => {
+  if (type === 'name') {
+    return {
+      id: 'name',
+      type: 'text',
+      label: 'NOMBRE *',
+      placeholder: 'Escribe tu nombre'
+    }
+  } else if (type === 'email') {
+    return {
+      id: 'email',
+      type: 'email',
+      label: 'CORREO ELECTRÓNICO *',
+      placeholder: 'Escribe tu correo'
+    }
+  } else {
+    return {
+      id: 'phone',
+      type: 'tel',
+      label: 'TELÉFONO *',
+      placeholder: 'Escribe tu teléfono'
+    }
+  }
+}
+
 const Trabajo = () =>
   <div className="work-box row">
     <WorkCard />
@@ -25,8 +50,53 @@ const WorkCard = () =>
   </div>
 
 const WorkForm = () =>
-  <div className="work-form column">
+  <div className="work-form">
+    <div className="rect"></div>
+    <header>FORMULARIO</header>
+    <div className="column">
+      <Input variant="name" />
+      <Input variant="email" />
+      <Input variant="phone" />
+      <Submit />
+      <TextArea />
+      <button className="submit">
+        ENVIAR
+      </button>
+    </div>
+  </div>
 
+const Input = ({variant}) => {
+  const input = getInputDetails(variant)
+  return(
+    <div className="work-form-input column">
+      <label htmlFor={input.id} className="work-form-labels">
+        {input.label}
+      </label>
+      <input
+        type={input.type}
+        placeholder={input.placeholder}
+        id={input.id}
+      />
+    </div>
+  )
+}
+
+const Submit = () =>
+  <div className="work-form-submit column">
+    <p className="work-form-labels">
+      ADJUNTA TU CV
+    </p>
+    <button>Adjuntar archivo</button>
+  </div>
+
+const TextArea = () =>
+  <div className="work-form-textarea column">
+    <label htmlFor="textarea" className="work-form-labels">
+      ¿POR QUÉ QUIERES UNIRTE A NOSOTROS?
+    </label>
+    <textarea
+      id="textarea"
+    />
   </div>
 
 export default Trabajo
